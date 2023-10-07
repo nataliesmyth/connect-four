871c4e5f3a931149d60a162533ea66e8b1792c40
Nataliesmyth <nataliesmyth11@gmail.com>
2023-10-07 12:48:00 -0400

    switch timer container to yellow or red based on whose turn it is

--- game/scripts/game-logic.js
+++ game/scripts/game-logic.js
@@ -3,6 +3,11 @@
 let playerRed = 'r'
 let playerYellow = 'y'
 let currentPlayer = playerRed;
+let playerOne = playerRed;
+let playerTwo = playerYellow
+let count = 5;
+let redWinTotal = 0;
+let yellowWinTotal = 0;
 const trackFirstPlayer = [];
 
 let gameOver = false;
@@ -17,25 +22,33 @@ let currentTime = 5;
 // ----- Cached DOM Elements ----- //
 const timerText = document.getElementById('timer');
 let restartGame = document.getElementById('restartBtn');
-let redWinCount = document.getElementById('red-win-count')
-let yellowWinCount = document.getElementById('yellow-win-count')
-let currPlayerText = document.getElementById('current-player')
-let count = 5;
-let redWinTotal = 0;
-let yellowWinTotal = 0;
+let playAgainBtn = document.getElementById('playAgainBtn');
+let redWinCount = document.getElementById('red-win-count');
+let yellowWinCount = document.getElementById('yellow-win-count');
+let currPlayerText = document.getElementById('current-player');
 let menu = document.getElementById('game-menu');
-let timerContainer = document.getElementById('timerContainer')
+let timerContainerRed = document.getElementById('timerContainerRed')
+let timerContainerYellow = document.getElementById('timerContainerYellow')
 let timerBackground = document.getElementById('timerBackground')
 let winnerContainer = document.getElementById('winnerContainer')
 let winnerBackground = document.getElementById('winnerBackground');
 
 restartGame.addEventListener('click', handleRestartClick);
-
+playAgainBtn.addEventListener('click', playAgain)
 window.onload = function() {
     setGame()
 }
 
 const intervalID = setInterval(setTimer, 1000);
+
+function playAgain() {
+    document.getElementById('board').innerHTML = '';
+    gameOver = false;
+    handlePlayerOne();
+    playerRedState();
+    setGame();
+
+}
 
 function setGame() {
     if (trackFirstPlayer.length % 2 === 0) {
@@ -89,16 +102,20 @@ function setPiece() {
     }
 
     board[r][c] = currentPlayer;
+    console.log(currentPlayer)
     let tile = document.getElementById(r.toString() + '-' + c.toString());
     // console.log(tile)
     if (currentPlayer == playerRed) {
-        
         tile.classList.add('red-piece')
         clearInterval(intervalID)
-        currentPlayer = playerYellow
+        currentPlayer = playerYellow;
+        // timerContainerRed.display = none;
+        // timerContainerYellow.display = block;
+        playerYellowState();
     } else {
         tile.classList.add('yellow-piece')
-        currentPlayer = playerRed
+        currentPlayer = playerRed;
+        playerRedState();
     }
     // setInterval(setTimer, 1000);
 
@@ -183,18 +200,19 @@ function setWinner(r, c) {
     if (board[r][c] == playerRed) {
         redWinTotal++
         winner.innerText = 'player 1'
-        redWinCount.innerText = redWinTotal
+        redWinCount.innerText = redWinTotal;
+        redWinState();
     } else if (board[r][c] == playerYellow) {
         yellowWinTotal++
         winner.innerText = 'player 2'
         yellowWinCount.innerText = yellowWinTotal;
+        yellowWinState();
     }
     console.log('winner! play again?')
 
     // console.log(winner)
 
     gameOver = true;
-    redWinState()
     // handleRestart()
     // setGame()
 }
@@ -235,16 +253,43 @@ function setWinner(r, c) {
         }
     }
 
+    function playerState() {
+        // if it's player red's turn, player red state is shown
+        // if it's player yellow's turn, player yellow state is shown
+    }
+
+    function playerWinState() {
+
+    }
+
+    function playerRedState() {
+        timerContainerRed.style.display = 'block';
+        timerBackground.style.display = 'block';
+        timerContainerYellow.style.display = 'none';
+        winnerContainer.style.display = 'none';
+        winnerBackground.style.display = 'none';
+    }
+
+    function playerYellowState() {
+        timerContainerYellow.style.display = 'block';
+        timerBackground.style.display = 'block';
+        winnerContainer.style.display = 'none';
+        winnerBackground.style.display = 'none';
+    }
+
     function redWinState() {
         // if red wins, red win state is shown
-        timerContainer.style.display = 'none';
+        timerContainerRed.style.display = 'none';
         timerBackground.style.display = 'none';
         winnerContainer.style.display = 'flex';
         winnerBackground.style.display = 'block';
-        console.log('red win state')
     }
 
     function yellowWinState() {
         // if yellow wins, yellow win state is shown
         console.log('yellow win state')
+        // timerContainerYellow.style.display = 'block';
+        timerBackground.style.display = 'block';
+        winnerContainer.style.display = 'none';
+        winnerBackground.style.display = 'none';
     }
